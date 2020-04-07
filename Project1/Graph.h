c915d8fcc4a69b85f1e697160a71e104eb34e4dd
matias francois <matiasf1997@hotmail.com>
2020-04-07 13:29:22 -0300

    mas cambios menores

--- Project1/Graph.h
+++ Project1/Graph.h
@@ -35,6 +35,13 @@ public:
 
 private:
     int inicializa(void);
+    int seteo(ALLEGRO_DISPLAY* display);
+    int carga_bitmaps(void);
+    void VentanaPrincipal(void);
+    void Pregunta(char* texto_ingresado);
+    void cleaner(ALLEGRO_DISPLAY* ventana_a_borrar);
+    void clean(ALLEGRO_DISPLAY* ventana_a_borrar);
+    bool Ventanainicio(void);
     ALLEGRO_DISPLAY* display_principal;
     ALLEGRO_DISPLAY* display_entrada;
     ALLEGRO_SAMPLE* select_sample;
@@ -43,30 +50,22 @@ private:
     ALLEGRO_BITMAP* food;
     ALLEGRO_BITMAP* grownBlob;
     ALLEGRO_BITMAP* goodOldBlob;
-    //crea el evento
     ALLEGRO_EVENT_QUEUE* queue;
+    ALLEGRO_EVENT ev;
     bool close_display;
     bool show_demo_window;
-    float Death[3];
-    float velp;
-    float smellRadius;
-    int foodCount;
-    float randomJiggleLimit;
-    int modo;
-    float Vel_max;
-    int cant_inicial_blobs;
     bool cerrar;
     bool close_window;
     bool show_another_window;
     bool running;
     bool running_inicio;
-    ALLEGRO_EVENT ev;
-    void VentanaPrincipal(void);
-    int seteo(ALLEGRO_DISPLAY* display);
-    int carga_bitmaps(void);
-    void Pregunta(char* texto_ingresado);
-    void cleaner(ALLEGRO_DISPLAY* ventana_a_borrar);
-    void clean(ALLEGRO_DISPLAY* ventana_a_borrar);
-    bool Ventanainicio(void);
+    float Death[3];
+    float velp;
+    float smellRadius;
+    float randomJiggleLimit;
+    float Vel_max;
+    int foodCount;
+    int modo;
+    int cant_inicial_blobs;
 };
 
